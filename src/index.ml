b70c0e5bb04bb33b5352ba4d85e3c6fbd3d65958
Jake F - @samouri <samouri@users.noreply.github.com>
2018-05-21 18:54:54 -0400

    gaining some context

--- src/index.ml
+++ src/index.ml
@@ -1,25 +1,17 @@
-open Webapi.Dom;;
-
-let appDiv = document |> Document.getElementById "app";;
-
-(* ((map Element.setInnerHTML appDiv) "<h1>Buckle Down Baby</h1") *)
-
-let sleep n = Js.Promise.make (fun ~resolve ~reject:_ -> 
-    let _ : Js.Global.timeoutId =
-      Js.Global.setTimeout
-        (fun () -> (resolve "Done!")[@bs])
-        n
-    in ()
-  );;
-
-let sol1 = 
-  let sum = ref 0 in 
-  for i = 0 to 999 do 
-    if i mod 3 = 0 || i mod 5 = 0 then sum := !sum + i;
-    Js.log i;
-  done;
-  sum;;
-
-
-
-Js.log sol1
+open Webapi.Dom
+type dimensions = {
+  height: int;
+  width: int;}
+let canvasDimensions: dimensions = { height = 400; width = 760 }
+let load _ =
+  let canvas_id = "canvas" in
+  let canvas =
+    match document |> (Document.getElementById "canvas") with
+    | None  ->
+        (print_endline ("cant find canvas " ^ (canvas_id ^ " \n"));
+         failwith "fail")
+    | Some el -> el in
+  let context = CanvasRe.CanvasElement.getContext2d canvas in
+  Canvas2dRe.setFillStyle context String "red";
+  Canvas2dRe.fillRect context ~x:0. ~y:0. ~h:100. ~w:100.
+let _ = Window.setOnLoad window load
